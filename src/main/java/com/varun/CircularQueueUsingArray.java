0bf389c748e7fccd8e02d8033deb3a1c0af8227e
varun <varunpawardigital@gmail.com>
2026-02-11 09:22:18 +0530

    updated repo with new data structures

---
+++ src/main/java/com/varun/CircularQueueUsingArray.java
@@ -0,0 +1,71 @@
+package com.varun;
+
+public class QueueUsingArray {
+    int front;
+    int rear;
+    int capacity;
+    int size;
+    String[] arr;
+
+    public QueueUsingArray(int capcity) {
+        this.front = 0;
+        this.rear = -1;
+        this.capacity = capcity;
+        this.size = 0;
+        this.arr = new String[capcity];
+    }
+
+
+    public void enqueue(String data) {
+
+
+        if (arr[front] == null) {
+            arr[front] = data;
+            size++;
+            return;
+        }
+
+        try {
+            if (size > capacity) {
+                throw new Exception("Queue Full");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        if (size < capacity) {
+            arr[size] = data;
+            size++;
+        }
+
+    }
+
+    public void dequeue(){
+
+        try {
+            if (arr[front] == null) {
+             throw new Exception("Que is Empty");
+            }
+
+            arr[front] = null;
+            // when front is deleting the last element the pointers should be reset.
+            front = (front + 1) % capacity;
+            rear = (rear + 1) & capacity;
+            size--;
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public boolean isEmpty(){
+        return arr[front] == null;
+    }
+
+    public String front(){
+        if (arr[front] != null) {
+            return arr[front];
+        }
+        return null;
+    }
+}
---
+++ src/test/java/com/varun/CircularQueueUsingArrayTest.java
@@ -0,0 +1,54 @@
+package com.varun;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QueueUsingArrayTest {
+    QueueUsingArray q1;
+
+    @BeforeEach
+    public void setUp(){
+        q1 = new QueueUsingArray(3);
+    }
+
+
+    @Test
+    public void enqueueDequeTest(){
+        q1.enqueue("manisha");
+        assertEquals("manisha" , q1.front());
+        q1.enqueue("varun");
+        assertEquals("manisha" , q1.front());
+        q1.enqueue("my wife");
+        assertEquals("manisha" , q1.front());
+
+
+        q1.dequeue();
+        assertEquals("varun" , q1.front());
+
+        q1.dequeue();
+        assertEquals("my wife" , q1.front());
+
+        q1.dequeue();
+        assertNull(q1.front());
+
+
+        assertTrue(q1.isEmpty());
+
+        q1.enqueue("manisha");
+        assertEquals("manisha" , q1.front());
+        q1.enqueue("varun");
+        assertEquals("manisha" , q1.front());
+
+        q1.dequeue();
+        assertEquals("varun" , q1.front());
+
+        q1.dequeue();
+        assertNull(q1.front());
+
+
+
+    }
+}
